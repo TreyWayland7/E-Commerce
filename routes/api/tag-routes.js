7048eb635e9be3650f2a286bd04ec30fce0ee208
Trey Wayland <waylandtrey7@gmail.com>
2024-08-27 15:26:28 -0700

    added tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -22,21 +22,89 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+
+  try{
+    const tags = await Tag.findAll({
+      where: { id: req.params.id },
+      include: [
+        {model: Product}
+      ]
+
+    })
+
+    res.json(tags);
+
+  }catch(err){
+    console.error(err);
+    res.status(500).json({error: 'Failed to find tags'})
+  }
+
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+
+ try{
+  const {tag_name} = req.body;
+  const tag = await Tag.create({tag_name});
+  res.status(201).json(tag);
+  }catch(err){
+    console.error(err);
+    res.status(500).json({error: 'Failed to find tags'})
+  }
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try{
+    const {tag_name} = req.body;
+
+    const id = req.params.id;
+
+    const didItwork = await Tag.update(
+      {tag_name},
+      {where: {id}, returning: true },
+
+    );
+
+    if (didItwork === 0){
+      res.status(404).json({error: 'Failed to find tags'})
+    }
+    const updatedTag = await Tag.findByPk(id);
+    res.json(updatedTag);
+
+    }catch(err){
+      console.error(err);
+      res.status(500).json({error: 'Failed to find tags'})
+    }
+
 });
 
-router.delete('/:id', (req, res) => {
+
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try{
+    const id = req.params.id;
+
+    const didItwork = await Tag.destroy({
+      where: {id},
+    });
+
+    if (didItwork === 0){
+      res.status(404).json({error: 'Failed to find tags'})
+    }
+    res.status(404).json({message: 'Delete success.'});
+
+    }catch(err){
+      console.error(err);
+      res.status(500).json({error: 'Failed to find tags'})
+    }
+
+
 });
 
 module.exports = router;
