d4b73411281fbfa0b1d06ca93d0dcd09c1112d87
Trey Wayland <waylandtrey7@gmail.com>
2024-08-27 14:47:42 -0700

    init commit of files and added models.

---
+++ models/Product.js
@@ -0,0 +1,69 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+
+        validate: {
+            min: 0.01,
+            isDecimal: true,
+            customValidator(value){
+                const valueParsed = parseFloat(value).toFixed(2);
+                const value_string = value.toString();
+                if (valueParsed !== value_string){
+                    throw new Error('Decimal value error.');
+                }
+            }
+        }
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+        validate: {
+            isInt: true,
+            min: 0,
+            customNumeric(value){
+                if(!Number.isInteger(value)){
+                    throw new Error('Integer value error.');
+                }
+            }
+        }
+    },
+    category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'category',
+            key: 'id'
+        }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Product;
