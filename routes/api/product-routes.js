5115ab794268c20e1ae5c481514db59cb1d6be4e
Trey Wayland <waylandtrey7@gmail.com>
2024-08-27 15:54:43 -0700

    added product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,45 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try{
+    const tags = await Product.findAll({
+      include: [
+        {model: Category},{model: Tag}
+      ]
+
+    })
+
+    res.json(tags);
+
+  }catch(err){
+    console.error(err);
+    res.status(500).json({error: 'Failed to find product'})
+  }
+
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try{
+    const tags = await Product.findAll({
+      where: { id: req.params.id },
+      include: [
+        {model: Category},{model: Tag}
+      ]
+
+    })
+
+    res.json(tags);
+
+  }catch(err){
+    console.error(err);
+    res.status(500).json({error: 'Failed to find product'})
+  }
 });
 
 // create new product
@@ -92,8 +122,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try{
+    const id = req.params.id;
+
+    const didItwork = await Product.destroy({
+      where: {id},
+    });
+
+    if (didItwork === 0){
+      res.status(404).json({error: 'Failed to find product'})
+    }
+    res.status(404).json({message: 'Delete success.'});
+
+    }catch(err){
+      console.error(err);
+      res.status(500).json({error: 'Failed to find tags'})
+    }
 });
 
 module.exports = router;
